710ae5443858457899172777688316dca9c41a36
parth-mn <parth.nair27@gmail.com>
2026-07-18 00:12:04 +0530

    flipbook linked

--- components/header.js
+++ components/header.js
@@ -79,7 +79,7 @@ class Header extends HTMLElement {
     <a href="about">About</a>
     <a href="contact">Contact</a>
 </div>
-<a class="nav-cta link-cta" href="magazine">Read Issue 01</a>
+<a class="nav-cta link-cta" href="https://heyzine.com/flip-book/issue01july18.html">Read Issue 01</a>
 </nav>
 </header>`;
 
