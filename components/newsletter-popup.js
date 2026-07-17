500e2ba119b2b4997942611e64ea5f65acc8ed35
parth-mn <parth.nair27@gmail.com>
2026-07-17 23:48:36 +0530

    popup working

--- components/newsletter-popup.js
+++ components/newsletter-popup.js
@@ -1,3 +1,22 @@
+// const modal = document.getElementById("newsletter-modal");
+// console.log(window.location.pathname);
+// console.log(localStorage.getItem("newsletterSeen"));
+
+// if (
+//     window.location.pathname.endsWith("/") ||
+//     window.location.pathname.endsWith("index.html")
+// ) {
+//     console.log("Homepage detected");
+
+//     if (!localStorage.getItem("newsletterSeen")) {
+//         console.log("Showing popup");
+
+//         setTimeout(() => {
+//             modal.classList.remove("hidden");
+//         }, 2500);
+//     }
+// }
+// console.log("script loaded xddddddd");
 const modal = document.getElementById("newsletter-modal");
 if(modal){
     if (
