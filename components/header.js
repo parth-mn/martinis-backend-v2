c7994ffd10ec01a2a404f848847db605591d7469
parth-mn <parth.nair27@gmail.com>
2026-08-22 15:18:21 +0530

    issue 03 edits

--- components/header.js
+++ components/header.js
@@ -117,7 +117,7 @@ class Header extends HTMLElement {
     <a href="about">About</a>
     <a href="contact">Contact</a>
 </div>
-<a class="nav-cta link-cta" href="https://heyzine.com/flip-book/issue02august08.html">Read Issue 02</a>
+<a class="nav-cta link-cta" href="https://heyzine.com/flip-book/issue03august22.html">Read Issue 03</a>
 <button class="menu-toggle" aria-label="Open menu">
     ☰
 </button>
